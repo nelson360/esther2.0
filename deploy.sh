ded1ec864c591ead9f139e2c5c36297e1d4d0a74
Nelson Omondi <nelsonomondi99@gmail.com>
2022-11-18 02:14:45 +0300

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+set - e
+
+npm run build
+
+cd dist
+
+dit init
+git add - A
+git commit -m'New deployment'
+
+git push -f git@github.com:nelson360/esther2.0.git master:gh-pages
+
+cd -
